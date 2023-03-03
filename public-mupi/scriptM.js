d7c48b2e857a9cdeb1c956d03a83953abd74e2f7
Ana Agudelo <98620510+Agudelo02093@users.noreply.github.com>
2023-03-03 00:52:57 -0500

    con mupi

--- public-mupi/scriptM.js
+++ public-mupi/scriptM.js
@@ -6,6 +6,16 @@ console.log('Server IP: ', NGROK);
 
 socket.on('display-score', msn => {
     console.log(msn);
+    document.documentElement.innerHTML = `
+    <link rel="stylesheet" href="./style.css">
+    <div class="scoreContainer">
+        <img src="./imgMupi/red-logo.svg" alt="logo" class="logo">
+        <h4>Keep going!</h4>
+        <h1>Score</h1>
+        <h3>${msn}</h3>
+        <progress id="barra" class="progress-bar bg-danger" value="${msn}" max="20"></progress>
+    </div>
+    `
 });
 
 socket.on('display-datas', email => {
